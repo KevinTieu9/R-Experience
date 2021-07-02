7ca300ec36c77e3f7f5d812d10698927bf3ae499
Kevin <kt216@humboldt.edu>
2021-07-01 19:30:09 -0700

    Added First R file
    Used for learning syntax as well as vectors and arithmetic operations of R.

---
+++ VectorAndArithmeticLesson.R
@@ -0,0 +1,20 @@
+x <- 5 + 7
+x
+
+y <- x - 3
+y
+
+z <- c(1.1, 9, 3.14)
+z
+c(z, 555, z)
+z * 2 + 100
+c(1, 2, 3, 4) + c(0, 10)
+
+my_sqrt <- sqrt(z-1)
+my_sqrt
+
+my_div <- z/my_sqrt
+my_div
+
+my_div + my_sqrt
+
